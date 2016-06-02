8e4d347ff17a9529d34115f1cc5fcee1c4f8e72f
Chandrasekhar Allam <chandrasekhar.allam@mutualmobile.com>
2016-06-02 15:11:58 +0530

    Implemented ToDo app which store data in Firebase and use GAE to send todo task email everyday

--- backend/src/main/java/com/example/todo/backend/MyServlet.java
+++ backend/src/main/java/com/example/todo/backend/MyServlet.java
@@ -1,31 +1,74 @@
-/*
-   For step-by-step instructions on connecting your Android application to this backend module,
-   see "App Engine Java Servlet Module" template documentation at
-   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloWorld
-*/
-
 package com.example.todo.backend;
 
-import java.io.IOException;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyServlet extends HttpServlet {
+    static Logger Log = Logger.getLogger("com.example.[USERNAME].myapplication.backend.MyServlet");
+    private String url = "https://todo-eb54e.firebaseio.com/todoItems";
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
+        System.out.println("=============Get");
+        // Log.info("Got cron message, constructing email.");
         resp.setContentType("text/plain");
-        resp.getWriter().println("Please use the form to POST to this url");
-    }
+        resp.getWriter().println("Hello World!!");
 
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        String name = req.getParameter("name");
-        resp.setContentType("text/plain");
-        if (name == null) {
-            resp.getWriter().println("Please enter a name");
-        }
-        resp.getWriter().println("Hello " + name);
+        //Create a new Firebase instance and subscribe on child events.
+        Firebase firebase = new Firebase(url);
+        firebase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Build the email message contents using every field from Firebase.
+                System.out.println("=============onDataChange");
+                final StringBuilder newItemMessage = new StringBuilder();
+                newItemMessage.append("Good Morning!  You have the following todo items:\n");
+                for (DataSnapshot todoItem : dataSnapshot.getChildren()) {
+                    for (DataSnapshot field : todoItem.getChildren()) {
+                        newItemMessage.append(field.getKey())
+                                .append(":")
+                                .append(field.getValue().toString())
+                                .append("\n");
+                    }
+                }
+
+                //Now Send the email
+                Properties props = new Properties();
+                Session session = Session.getDefaultInstance(props, null);
+                try {
+                    Message msg = new MimeMessage(session);
+                    //Make sure you substitute your project-id in the email From field
+                    msg.setFrom(new InternetAddress("ToDo@todo-eb54e.appspotmail.com",
+                            "Todo Nagger"));
+                    msg.addRecipient(Message.RecipientType.TO,
+                            new InternetAddress("shekarrex@gmail.com", "Recipient"));
+                    msg.setSubject("Good Morning!");
+                    msg.setText(newItemMessage.toString());
+                    Transport.send(msg);
+                } catch (javax.mail.MessagingException | UnsupportedEncodingException e) {
+                    //  Log.warning(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
     }
 }
